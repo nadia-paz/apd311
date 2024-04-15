7be6bba5843e7bcdeb6117f08aacdcffe6ce8ccd
Nadia Paz <nadina.abouhamdan@gmail.com>
2024-04-14 21:12:46 -0500

    split DAG into 2

--- airflow/dags/pipeline.py
+++ airflow/dags/pipeline.py
@@ -1,8 +1,8 @@
 from datetime import timedelta, datetime
 
-
 # airflow imports
 from airflow import DAG
+#from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
@@ -10,28 +10,52 @@ from airflow.operators.python import PythonOperator
 # from google.cloud import storage
 # from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
 # from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
+
 import functions as f
 
 
-
 default_args = {
-    'owner': 'airflow',
-    'retries': 2,
-    'retry_delay': timedelta(minutes=1)
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 2,
+    'retry_delay': timedelta(minutes=5)
 }
 # scheduler cron notation
 # Minute Hour Day(month) Month Day(week)
 
+"""  
+Creates  DAGs.
+1. dag1 -> scheduled to perform only once, loads a spark file into a Google Cloud Storage bucket
+2. dag2 -> the main data DAG, scheduled to run weeakly each Sunday at midnight til June, 30 2024:
+            * loads data from API and saves into GCS
+            * creates a Dataproc cluster
+            * submits a spark job for data transformations
+            * deletes a Dataproc cluster
+"""
+
 with DAG(
-    dag_id='pipeline_v0',
+    dag_id='upload_spark_file',
     default_args=default_args,
-    start_date=datetime(2024, 4, 10),
+    start_date=datetime(2024, 4, 12),
+    schedule='@once',
+    catchup=False
+) as dag1:
+    spark_job_file_task = PythonOperator(
+        task_id = 'spark_job_file_task', 
+        python_callable = f.spark_job_file
+    )
+
+with DAG(
+    dag_id='pipeline_v01',
+    default_args=default_args,
+    start_date=datetime(2024, 4, 12),
     # every Sunday at 00:00
     end_date=datetime(2024, 6, 30),
     schedule='0 0 * * SUN'
-) as dag:
-
-    # saves data from Austin Public Data API as parquet files on GCS
+) as dag2:
+    # load data to GCS
     save_data_task = PythonOperator(
         task_id = 'save_data_task',
         python_callable = f.save_data,
@@ -42,19 +66,13 @@ with DAG(
         }
     )
 
-    # loads spark file for data transformation into GCS
-    spark_job_file_task = PythonOperator(
-        task_id = 'spark_job_file_task', # change to dag -> only once
-        python_callable = f.spark_job_file
-    )
-
-    # creates DataProc cluster on GCS
+    # create a Dataproc cluster
     create_cluster_task = PythonOperator(
         task_id = 'create_cluster_task',
         python_callable = f.create_dataproc_cluster
     )
 
-    # submits spark job on Dataproc
+    # submit spark job to Dataproc
     submit_spark_job_task = BashOperator(
         task_id = 'submit_spark_job_task',
         bash_command = f"""
@@ -67,14 +85,13 @@ with DAG(
         """
     )
 
-    # deletes a cluster
+    # delete Dataproc cluster
     delete_cluster_task = PythonOperator(
         task_id = 'delete_cluster_task',
         python_callable = f.delete_dataproc_cluster
     )
 
     save_data_task >> \
-        spark_job_file_task >> \
-            create_cluster_task >> \
-                submit_spark_job_task >> \
-                    delete_cluster_task
+        create_cluster_task >> \
+            submit_spark_job_task >> \
+                delete_cluster_task
